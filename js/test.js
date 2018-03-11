b33eff9d562e5ff190862c25916f7958e18b6f46
Prakash Giri <prakashgiri.2120@gmail.com>
2018-03-11 13:37:37 +0530

    changed

--- js/test.js
+++ js/test.js
@@ -1,4 +1,8 @@
 
+$(function(){
+  workBelt();
+});
+
 $(window).scroll(function(){
 
   var wScroll = $(this).scrollTop();
@@ -14,3 +18,35 @@ $(window).scroll(function(){
     'transform': 'translate(0px,'+ wScroll/4+'%)'
   });
 });
+
+$(function(){
+  $('.signup-b').click(function(){
+    $('.login-box').animate({left:"0%"},400);
+    $('.login-box').animate({left:"4%"},400);
+    $('.login_form').css("visibility","hidden");
+    $('.signup_form').css("visibility","visible");
+
+    $('.signup_form').animate({left:"0%"},400);
+    $('.signup_form').animate({left:"4%"},400);
+    $('.login_form').animate({left:"4%"},400);
+  });
+  $('.login-b').click(function(){
+    $('.login-box').animate({left:"51%"},400);
+    $('.login-box').animate({left:"45%"},400);
+    $('.signup_form').css("visibility","hidden");
+    $('.login_form').css("visibility","visible");
+
+    $('.login_form').animate({left:"51%"},400);
+    $('.login_form').animate({left:"45%"},400);
+    $('.signup_form').animate({left:"45%"},400);
+
+
+  });
+});
+
+//workBelt
+function workBelt(){
+  $('thumbnail').click(function(){
+    $('.work-belt').css({'left':'-100%'});
+  });
+}
